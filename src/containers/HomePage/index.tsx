1c570cce74fd3b71b77471d5b26daa37c5bded18
peuks <vanmakdavid.lwg@gmail.com>
2021-12-01 21:19:46 +0100

    INIT: Initial Commit

---
+++ src/containers/HomePage/index.tsx
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+import tw from "twin.macro";
+
+interface Props {}
+
+const PageContainer = styled.div`
+  ${tw`
+  
+  `}
+`;
+
+const HomePage = (props: Props) => {
+  return (
+    <PageContainer>
+      <h1>yoyo</h1>
+    </PageContainer>
+  );
+};
+
+export default HomePage;
